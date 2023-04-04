6fbf0e535a74dfd5773bc202e297dd5b6191be3c
Feyza Basaran <basa0044@algonquinlive.com>
2023-04-04 02:04:55 -0400

    added a form to update contact in edit component

--- contact-book/src/views/Edit.vue
+++ contact-book/src/views/Edit.vue
@@ -1,10 +1,46 @@
 <template>
     <div>
-
+        <router-link :to="{ name: 'Contacts'}"><button><i>Arrow</i> Contacts</button></router-link>
+        <h1>Edit Contact</h1>
+        <form @submit.prevent>
+                <input 
+                type="text" 
+                id="fname" 
+                name="fname" 
+                placeholder="First Name"
+                v-model="fname">
+                <br>
+                <input 
+                type="text" 
+                id="lname" 
+                name="lname" 
+                placeholder="Last Name"
+                v-model="lname"
+                >
+                <br>
+                <input 
+                type="text" 
+                id="email" 
+                name="email" 
+                placeholder="email"
+                v-model="email"
+                >
+                <br>
+                <input 
+                type="text" 
+                id="phone" 
+                name="phone" 
+                placeholder="Phone"
+                v-model="phone"
+                >
+                <br>
+                <button type="submit">Update Contact</button>
+            </form>
+            <button>Delete Contact</button>
     </div>
 </template>
 
-<script setup>
+<script>
 
 </script>
 
